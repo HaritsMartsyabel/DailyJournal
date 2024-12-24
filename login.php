1d92cfb35d734df0a76aff5b4f38cc364e3dccab
HaritsMartsyabel <abeljepara5454@gmail.com>
2024-12-24 13:46:58 +0700

    ubah judul form login

--- login.php
+++ login.php
@@ -66,7 +66,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <div class="card-body">
           <div class="text-center mb-3">
             <i class="bi bi-person-circle h1 display-4"></i>
-            <p>Real Madrid</p>
+            <p>  Welcome to Real Madrid</p>
             <hr />
           </div>
           <form method="post">
